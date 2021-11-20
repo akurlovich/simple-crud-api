b4ee7c514b509604e0e110609139110348b31341
akurlovich <71250705+akurlovich@users.noreply.github.com>
2021-11-20 23:02:47 +0300

    feat: url path

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 const http = require('http');
 const { PORT } = require('./config.js');
 
-http.createServer(function(req, res){
-  res.end('Hello')
+http.createServer(function (req, res) {
+  res.setHeader("Content-Type", "text/html; charset=utf-8;");
+  if (req.url === "/") {
+    res.write("<h2>Home</h2>");
+  }
+  else if (req.url == "/person") {
+    res.write("<h2>Person</h2>");
+  }
+  else if (req.url == "/person/:id") {
+    res.write("<h2>Person ID</h2>");
+  }
+  else {
+    res.statusCode = 404;
+    res.write("<h2>Page not found</h2>");
+  }
+  // res.write("<h1>Hello!!!</h1>");
+  res.end();
 }).listen(PORT, () => {
   console.log(`Server start on port ${PORT}`)
 })
