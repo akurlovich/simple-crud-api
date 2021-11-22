4de9f9ae042150c2f7ed8be54336c2b9b995cfaf
akurlovich <71250705+akurlovich@users.noreply.github.com>
2021-11-22 23:12:09 +0300

    fix: webpack prod config

--- webpack.common.js
+++ webpack.common.js
@@ -1,24 +1,28 @@
 const path = require('path');
- const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
- module.exports = {
-   entry: {
-     app: './src/index.js',
-   },
-   resolve: {
-    fallback: {
-        "http": require.resolve("stream-http"),
-        "buffer": require.resolve("buffer/")
-    },
+module.exports = {
+  target: 'node', 
+  externalsPresets: { node: true }, // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  entry: {
+    app: './src/index.js',
   },
-   plugins: [
-     new HtmlWebpackPlugin({
-       title: 'Production',
-     }),
-   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-   },
- };
+  // resolve: {
+  //   fallback: {
+  //     "http": require.resolve("stream-http"),
+  //     "buffer": require.resolve("buffer/")
+  //   },
+  // },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Production',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
