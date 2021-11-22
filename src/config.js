1f0c86cddd9e5e047029067f57d1da9747455090
akurlovich <71250705+akurlovich@users.noreply.github.com>
2021-11-22 20:10:54 +0300

    feat: .env

--- src/config.js
+++ src/config.js
@@ -8,6 +8,4 @@ dotenv.config({
 module.exports = {
   PORT: process.env.PORT,
   NODE_ENV: process.env.NODE_ENV,
-  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
-  AUTH_MODE: process.env.AUTH_MODE === 'true'
 };
